94b28e14140f61f3dd645fbaf965729a911e296f
Daniel <danieljfeller@gmail.com>
2020-03-23 09:00:50 -0400

    Modified the dashboard structure and added a dark theme

--- NYC_covid19/app.R
+++ NYC_covid19/app.R
@@ -1,46 +1,57 @@
 library(tidyverse)
 library(sf)
 library(shiny)
+library(shinyWidgets)
 library(leaflet)
 library(shinydashboard)
+library(dashboardthemes)
 
-tract_shapes <- read_sf('shapefiles/nyc_census_tracts.shp')
+#############
+# load data #
+#############
 
-combined_tract_df <- read_tsv('data/census/combined_tract_population_2017_2018.tsv',
-                              col_types = cols(GEOID = col_character(), estimate = col_double(),
-                                               year = col_double(), popup = col_character()))
+# nyc shape files
+plot_df <- inner_join(
+  # nyc census data
+  read_sf('shapefiles/nyc_census_tracts.shp'),
+  # nyc census data
+  read_tsv('data/census/combined_tract_population_2017_2018.tsv',
+                      col_types = cols(GEOID = col_character(), estimate = col_double(),
+                                       year = col_double(), popup = col_character())),
+  # join on GEOID
+  by = 'GEOID')
 
-plot_df <- tract_shapes %>%
-    inner_join(combined_tract_df, by = 'GEOID')
+########################
+# assign color palette #
+########################
 
+pal <- colorNumeric(palette = "YlOrRd", domain = plot_df$estimate, n = 10)
 
-pal <- colorNumeric(palette = "viridis", domain = plot_df$estimate, n = 10)
+# create dashboard Sidebar
 
-ui <- dashboardPage(
-    dashboardHeader(title = "DBMI COVID-19"),
-    dashboardSidebar(),  # Not sure what we might want here
-    dashboardBody(
+ui <- dashboardPage(skin = "black",
+    dashboardHeader(title = "NYC COVID Tracker"),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Map", tabName = "map", icon = icon("globe-americas")),
+        menuItem("Information", tabName = "info", icon = icon("info")),
+        menuItem("FAQ", icon = icon("question"), tabName = "faq",
+                 badgeLabel = "new", badgeColor = "green"),
+        chooseSliderSkin("Modern", 'black'),
+        sliderInput("date_slider",
+                    'Date',
+                    min = as.Date("2017-01-01"),
+                    max = as.Date("2018-01-01"),
+                    value = as.Date("2017-01-01"),
+                    timeFormat="%Y",
+                    step = 365),
+        selectInput('sex', 
+                    'Sex', 
+                    c('male', 'female', 'total')))),
+    dashboardBody(shinyDashboardThemes(theme = "grey_dark"),
         fluidRow(
-            box(width = 8,
-                title = "Date",
-                sliderInput("date_slider",
-                            NULL,
-                            min = as.Date("2017-01-01"),
-                            max = as.Date("2018-01-01"),
-                            value = as.Date("2017-01-01"),
-                            timeFormat="%Y",
-                            step = 365)
-            ),
-            column(4, selectInput('sex', 'Sex', c('male', 'female', 'total')))
-        ),
-        fluidRow(
-          box(
-              width = 8,
-              leafletOutput("nycmap", height = 600),
-          ),
-        )
-  )
-)
+          box(width = 12,
+              leafletOutput("nycmap", height = 600)))))
 
 
 server <- function(input, output) {
@@ -48,7 +59,7 @@ server <- function(input, output) {
     plot_df %>%
       filter(year == as.numeric(format(input$date_slider,'%Y')) & sex == input$sex) %>%
       leaflet(height = "100%") %>%
-      addProviderTiles(provider = "CartoDB.Positron") %>%
+      addProviderTiles(provider = "CartoDB.DarkMatter") %>%
       addPolygons(popup = ~ popup,
                   stroke = FALSE,
                   smoothFactor = 0,
