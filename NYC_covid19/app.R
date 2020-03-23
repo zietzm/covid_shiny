74473c87738c41b38594d6c43c08a5d1fdb8aff8
zietzm <michael.zietz@gmail.com>
2020-03-22 22:57:42 -0400

    Update map size and slider

--- NYC_covid19/app.R
+++ NYC_covid19/app.R
@@ -4,9 +4,9 @@ library(shiny)
 library(leaflet)
 library(shinydashboard)
 
-tract_shapes <- read_sf('shapefiles/nyc_census_tracts.shp')
+tract_shapes <- read_sf('../NYC_covid19/shapefiles/nyc_census_tracts.shp')
 
-combined_tract_df <- read_tsv('data/census/combined_tract_population_2017_2018.tsv',
+combined_tract_df <- read_tsv('../NYC_covid19/data/census/combined_tract_population_2017_2018.tsv',
                               col_types = cols(GEOID = col_character(), estimate = col_double(),
                                                year = col_double(), popup = col_character()))
 
@@ -14,34 +14,29 @@ plot_df <- tract_shapes %>%
     inner_join(combined_tract_df, by = 'GEOID')
 
 
-# Color palette range is wrong. Uses complete range even though only groups are
-#  shown at one time.
 pal <- colorNumeric(palette = "viridis", domain = plot_df$estimate, n = 10)
-
 
 ui <- dashboardPage(
     dashboardHeader(title = "DBMI COVID-19"),
-    dashboardSidebar(),  # Not sure what we might want in the left pane
+    dashboardSidebar(),  # Not sure what we might want here
     dashboardBody(
         fluidRow(
-            box(
+            box(width = 8,
                 title = "Date",
                 sliderInput("date_slider",
                             NULL,
                             min = as.Date("2017-01-01"),
                             max = as.Date("2018-01-01"),
                             value = as.Date("2017-01-01"),
-                            timeFormat="%Y")
+                            timeFormat="%Y",
+                            step = 365)
             ),
-            column(4,
-              selectInput('sex', 'Sex', c('male', 'female', 'total'))
-              # This would be the place for other data filters like race, comorbidities, etc.
-            )
+            column(4, selectInput('sex', 'Sex', c('male', 'female', 'total')))
         ),
         fluidRow(
           box(
-              width = 6,
-              leafletOutput("nycmap"),
+              width = 8,
+              leafletOutput("nycmap", height = 600),
           ),
         )
   )
@@ -57,7 +52,7 @@ server <- function(input, output) {
       addPolygons(popup = ~ popup,
                   stroke = FALSE,
                   smoothFactor = 0,
-                  fillOpacity = 0.7,
+                  fillOpacity = 0.4,
                   color = ~ pal(estimate)) %>%
       addLegend("bottomright",
                 pal = pal,
